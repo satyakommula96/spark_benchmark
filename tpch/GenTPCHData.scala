9ac3e19363c4fe385667895ea266334687111b0f
Satya Kommula <satyakommula@ymail.com>
2022-01-19 22:43:20 +0530

    add absolute path for datagenration for tpch

--- tpch/GenTPCHData.scala
+++ tpch/GenTPCHData.scala
@@ -3,7 +3,8 @@ import com.databricks.spark.sql.perf.tpch.TPCHTables
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
  
 val rootDir = "./parquet" // root directory of location to create data in.
-val dbgen = "../tpch-dbgen"  // location of dbgen
+val absolutePath = new java.io.File("../").getCanonicalPath //get absolute path
+val dbgen = absolutePath+"/tpch-dbgen"  // absolute location of dbgen
 val databaseName = "tpch" // name of database to create.
 val scaleFactor = "100" // scaleFactor defines the size of the dataset to generate (in GB).
 val format = "parquet" // valid spark format like parquet "parquet".
